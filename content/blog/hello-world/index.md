39d4b7a01b692674fe8fc24b5fb3bc716e051881
Marcin Krawczyk <marckraw@icloud.com>
2019-12-22 20:24:00 +0100

    Deplog blog and first post

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,19 @@
+---
+title: Hello, my name is Marcin...
+date: "2019-12-22T20:00:00"
+description: "Hello, my name is Marcin..."
+---
+
+This is the first post on this blog. I'm creating it to make myself accountable, write some great findings to remember, and to generaly share some maybe interesting things for you all :)
+
+For the beggining I hope to add here some value post weekly, but will not hesistate to add some more stuff wherever i feel like.
+
+Posts will be written in two languages. Polish (i am originally from Poland) and English. In future i will add some tags for you to easy find interesting topics or post languages. More important posts i will translate in both languages.
+
+Also, writing some stuff on the interent will help me with learning english anyway, so I'm looking forward to do that :)
+
+Graphic layout is just a simple basic one, from [Kyle Mathews](https://www.gatsbyjs.org/contributors/kyle-mathews/) magnificent founder of the great [GatsbyJS](https://www.gatsbyjs.org/).
+
+Definitelly I will design some stuff for this blog, to feel more 'at home' but, for now, i'm just rolling with this, to finally start.
+
+Well, that's it for now. Enjoy further reading, and I hope u will follow me with this journey.
---
+++ src/components/bio.js
@@ -0,0 +1,76 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+import { FaInstagram } from "react-icons/fa"
+
+import { rhythm } from "../utils/typography"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      avatar: file(absolutePath: { regex: "/marcin-codes.jpg/" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author
+          social {
+            twitter
+            instagram
+          }
+        }
+      }
+    }
+  `)
+
+  const { author, social } = data.site.siteMetadata
+  console.log(social)
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+      }}
+    >
+      <Image
+        fixed={data.avatar.childImageSharp.fixed}
+        alt={author}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          minWidth: 50,
+          borderRadius: `100%`,
+        }}
+        imgStyle={{
+          borderRadius: `50%`,
+        }}
+      />
+      <p>
+        Written by <strong>{author}</strong>.{" "}
+        <a
+          href={`https://www.instagram.com/${social.instagram}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <br />
+        Javascript Developer from <strong>Poland</strong>, living in{" "}
+        <strong>Switzerland</strong>
+      </p>
+    </div>
+  )
+}
+
+export default Bio
