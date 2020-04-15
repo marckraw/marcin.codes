ed47fa9af5f0e4233a7f6b209417ff11b7f9ac40
Marcin Krawczyk <marckraw@icloud.com>
2020-04-15 22:24:44 +0200

    Add Bulma for layout and other helper styles, add scss support

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,43 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
+const ExampleCardCompoennt = () => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src="https://bulma.io/images/placeholders/1280x960.png"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img
+              src="https://bulma.io/images/placeholders/96x96.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="media-content">
+          <p className="title is-4">John Smith</p>
+          <p className="subtitle is-6">@johnsmith</p>
+        </div>
+      </div>
+
+      <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+        iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{" "}
+        <a href="#">#responsive</a>
+        <br />
+        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      </div>
+    </div>
+  </div>
+)
+
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
@@ -16,6 +53,9 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
+
+        <ExampleCardCompoennt />
+
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
