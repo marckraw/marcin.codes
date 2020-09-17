14f1a8029f902be1c855a836671590915dd66828
Marcin Krawczyk <marckraw@icloud.com>
2020-09-17 18:46:15 +0200

    Test lint-stageD

--- src/storyblok-components/content-types/blog-post.sb.js
+++ src/storyblok-components/content-types/blog-post.sb.js
@@ -1,51 +1,48 @@
 module.exports = {
-    name: "blog-post",
-    display_name: "Blog Post",
-    is_nestable: false,
-    is_root: true,
-    "schema": {
-        "image": {
-            "type": "asset",
-            "filetypes": [
-                "images"
-            ],
-            "pos": 0
-        },
-        "title": {
-            "type": "text",
-            "pos": 1
-        },
-        "content": {
-            "type": "markdown",
-            "pos": 2
-        },
-        "tags": {
-            "type": "options",
-            "min_options": "",
-            "options": [
-                {
-                    "value": "frontend",
-                    "name": "frontend"
-                },
-                {
-                    "value": "backend",
-                    "name": "backend"
-                },
-                {
-                    "value": "non-tech",
-                    "name": "non-tech"
-                },
-                {
-                    "value": "devops",
-                    "name": "devops"
-                },
-                {
-                    "value": "",
-                    "name": ""
-                }
-            ],
-            "pos": 3
-        }
+  name: "blog-post",
+  display_name: "Blog Post",
+  is_nestable: false,
+  is_root: true,
+  schema: {
+    image: {
+      type: "asset",
+      filetypes: ["images"],
+      pos: 0,
     },
-}
-
+    title: {
+      type: "text",
+      pos: 1,
+    },
+    content: {
+      type: "markdown",
+      pos: 2,
+    },
+    tags: {
+      type: "options",
+      min_options: "",
+      options: [
+        {
+          value: "frontend",
+          name: "frontend",
+        },
+        {
+          value: "backend",
+          name: "backend",
+        },
+        {
+          value: "non-tech",
+          name: "non-tech",
+        },
+        {
+          value: "devops",
+          name: "devops",
+        },
+        {
+          value: "switzerland",
+          name: "switzerland",
+        },
+      ],
+      pos: 3,
+    },
+  },
+};
